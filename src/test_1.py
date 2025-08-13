9a0d7bbe6f5edb7f9c60b890def1ebc60c3647b3
Nikita <teslenko.20011@gmail.com>
2025-08-13 14:08:48 +0300

    repeat practice

--- src/test_1.py
+++ src/test_1.py
@@ -1,8 +1,3 @@
 # 1. Main
 def test(n):
-    """
-    step 3: writing docs
-    :return: list square numbers
-    """
-    # 2. dev_2: Тут мы описываем функциональность и пушим в main
-    return [i**2 for i in range(n)]
+    pass
